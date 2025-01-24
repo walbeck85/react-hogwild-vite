1bbef32efec92848e2f43890081a7728417f4664
Laura Berge <lauraeberge@gmail.com>
2025-01-24 10:53:06 -0500

    Update to jsx extension

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './src/__tests__/setup.js',
+    setupFiles: './src/__tests__/setup.jsx',
   }
 })
